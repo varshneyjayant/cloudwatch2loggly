51c0d7822d89c357e66fcda222e8b97e612032f6
psquickitjayant <jayantvarshney018@gmail.com>
2016-02-16 18:03:32 +0530

    Changed from pull to push
    We now support setting up event source for the Cloudwatch logs for a particular logstream instead of fetching all the logstream logs

--- cloudwatch2loggly.js
+++ cloudwatch2loggly.js
@@ -1,6 +1,7 @@
 var aws = require('aws-sdk')
   , Q = require('q') 
-  , request = require('request');
+  , request = require('request')
+  , zlib = require('zlib');
 
 //loggly url, token and tag configuration
 //user need to edit while uploading code via blueprint
@@ -17,142 +18,39 @@ var cloudWatchLogs = new aws.CloudWatchLogs({
 //entry point
 exports.handler = function (event, context) {
   
-  var nowDate = new Date();
-
-  //time upto which we want to fetch logs
-  var logEndTime = nowDate.getTime();
-
-  //time from which we want to fetch logs
-  var logStartTime = new Date(logEndTime - (5 * 60 * 1000)).getTime();
   var parsedEvents = [];
+  var totalLogs = 0;
 
-  //initiate the script here
-  getLogGroupsFromAWSCloudwatch().then(function () {
-    sendRemainingEvents().then(function () {
-      console.log('all events are sent to Loggly');
-      context.done();
-    }, function () {
-      context.done();
-    });
-  }, function () {});
+  sendEvents(event).then(function(){
+    context.succeed('all events are sent to Loggly');
+  }, function(){
+    context.done();
+  });
   
-  //retreives all the log groups present in the cloudwatch
-  function getLogGroupsFromAWSCloudwatch() {
-    var logStreamPromises = [];
-  
+  function sendEvents(event){
     return Q.Promise(function (resolve, reject) {
-      var getLogGroups = function (nextToken) {
-  
-        var logGroupParams = {};
-  
-        //log groups exceeds the count from 50, then next token should
-        //present to get the logs from next page
-        if (nextToken) {
-          logGroupParams.nextToken = nextToken;
+      var parseEventPromises = [];
+      var payload = new Buffer(event.awslogs.data, 'base64');
+      
+      zlib.gunzip(payload, function (error, result) {
+        if (error) {
+          context.fail(error);
+          reject();
+        } else {
+          var result_o = JSON.parse(result.toString('ascii'));
+          totalLogs = result_o.logEvents.length;
+
+          for (var i = 0; i < result_o.logEvents.length; i++) {
+            var parseEventPromise = parseEvent(result_o.logEvents[i], result_o.logGroup, result_o.logStream);
+            parseEventPromises.push(parseEventPromise);
+          }
+          Q.allSettled(parseEventPromises).then(function () {
+            resolve();
+          }, function () {
+            reject();
+          });
         }
-  
-        cloudWatchLogs.describeLogGroups(logGroupParams, function (err, result) {
-          if (err) {
-            console.log(err, err.stack);
-            resolve();
-          } else {
-            for (var count = 0; count < result.logGroups.length; count++) {
-              var logStreamPromise = fetchLogsStreamsFromLogGroup(result.logGroups[count].logGroupName);
-              logStreamPromises.push(logStreamPromise);
-            }
-  
-            if (result.nextToken) {
-              getLogGroups(result.nextToken);
-            } else {
-              Q.allSettled(logStreamPromises).then(function () {
-                resolve();
-              }, function () {
-                reject();
-              });
-            }
-          }
-        });
-      }
-      getLogGroups();
-    });
-  }
-  
-  //retrieves all the logstreams for a particular log group
-  function fetchLogsStreamsFromLogGroup(logGroupName) {
-    var logEventPromises = [];
-    return Q.Promise(function (resolve, reject) {
-      function getLogStreams(logGroupName, nextToken) {
-        var logStreamParams = {
-          descending : true,
-          logGroupName : logGroupName
-        };
-  
-        if (nextToken) {
-          logStreamParams.nextToken = nextToken;
-        }
-  
-        cloudWatchLogs.describeLogStreams(logStreamParams, function (err, result) {
-          if (err) {
-            console.log(err, err.stack);
-            resolve();
-          } else {
-            for (var count = 0; count < result.logStreams.length; count++) {
-              var logEventPromise = fetchEventsFromLogStream(logGroupName, result.logStreams[count].logStreamName);
-              logEventPromises.push(logEventPromise);
-            }
-  
-            if (result.nextToken) {
-              getLogStreams(logGroupName, result.nextToken);
-            } else {
-              Q.allSettled(logEventPromises).then(function () {
-                resolve();
-              }, function () {
-                reject();
-              });
-            }
-          }
-        });
-      };
-      getLogStreams(logGroupName);
-    });
-  }
-  
-  //retireves all the log events for a particular logstream inside a loggroup
-  function fetchEventsFromLogStream(logGroupName, logStreamName) {
-    var eventLogglyPromises = [];
-  
-    return Q.Promise(function (resolve, reject) {
-  
-      function getEvents(logGroupName, logStreamName) {
-  
-        //parameters to filter events
-        var eventParams = {
-          logGroupName : logGroupName,
-          logStreamName : logStreamName,
-          endTime : logEndTime,
-          startTime : logStartTime,
-          startFromHead : true
-        };
-  
-        cloudWatchLogs.getLogEvents(eventParams, function (err, result) {
-          if (err) {
-            console.log(err, err.stack);
-            resolve();
-          } else {
-            for (var count = 0; count < result.events.length; count++) {
-              var event = result.events[count];
-              var eventLogglyPromise = parseEvent(event, logGroupName, logStreamName);
-              eventLogglyPromises.push(eventLogglyPromise);
-            }
-            Q.allSettled(eventLogglyPromises).then(function () {
-              resolve();
-            }, function () {
-              reject();
-            });
-          }
-        });
-      }
-      getEvents(logGroupName, logStreamName);
+      });
     });
   }
   
@@ -166,48 +64,20 @@ exports.handler = function (event, context) {
         'logGroupName' : logGroupName,
         'logStreamName' : logStreamName,
         'timestamp' : new Date(event.timestamp).toISOString(),
-        'ingestionTime' : new Date(event.ingestionTime).toGMTString(),
       };
   
-      postEventToLoggly(eventData).then(function () {
-        resolve();
-      }, function () {
-        reject();
-      });
-  
-    });
-  }
-  
-  //uploads the events to Loggly
-  //we will hold the events in an array until they reaches to 100
-  //then set the count of zero.
-  function postEventToLoggly(event) {
-  
-    return Q.promise(function (resolve, reject) {
-      if (parsedEvents.length == 100) {
-        upload().then(function () {
+      parsedEvents.push(eventData);
+      
+      //let us wait for the all events to get added
+      //to the parseEvents array. Then we will send them to Loggly in one go
+      if(parsedEvents.length == totalLogs){
+        postEventToLoggly().then(function () {
           resolve();
         }, function () {
           reject();
         });
-      } else {
-        parsedEvents.push(event);
-        resolve();
       }
-    });
-  }
-  
-  //checks if any more events are left
-  //after sending events in multiples of 100
-  function sendRemainingEvents() {
-    return Q.promise(function (resolve, reject) {
-      if (parsedEvents.length > 0) {
-        upload().then(function () {
-          resolve();
-        }, function () {
-          reject();
-        });
-      } else {
+      else{
         resolve();
       }
     });
@@ -215,7 +85,7 @@ exports.handler = function (event, context) {
   
   //joins all the events to a single event
   //and sends to Loggly using bulk end point
-  function upload() {
+  function postEventToLoggly() {
     return Q.promise(function (resolve, reject) {
   
       //get all the events, stringify them and join them
@@ -223,12 +93,12 @@ exports.handler = function (event, context) {
       //via bulk endpoint
       var finalEvent = parsedEvents.map(JSON.stringify).join('\n');
   
-      //empty the main events array immediately to hold new events
+      //empty the main events array immediately
       parsedEvents.length = 0;
   
       //creating logglyURL at runtime, so that user can change the tag or customer token in the go
       //by modifying the current script
-      var logglyURL = logglyConfiguration.url + '/' + logglyConfiguration.customerToken + '/tag/' + logglyConfiguration.tags;
+      var logglyURL = logglyConfiguration.url + '/' + logglyConfiguration.customerToken + '/tag/' + encodeURIComponent(logglyConfiguration.tags);
   
       //create request options to send logs
       try {
@@ -237,7 +107,6 @@ exports.handler = function (event, context) {
           method : 'POST',
           headers : {}
         };
-  
         requestOptions.body = finalEvent;
   
         //now send the logs to Loggly
